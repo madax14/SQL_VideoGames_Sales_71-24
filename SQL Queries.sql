a3b61d46fc6e21292a118f42cb23e4ea90c6d82e
Diego Santos <120533129+madax14@users.noreply.github.com>
2024-07-26 00:49:20 -0300

    Update SQL Queries.sql
    First queries for cleaning and validating the data

--- SQL Queries.sql
+++ SQL Queries.sql
@@ -1,1 +1,25 @@
 
+##Checking for NULL values
+SELECT
+  #FORMAT_DATE('%Y', release_date) AS `Year`,
+  COUNT(*),
+  COUNT(title) AS `Game`,
+  COUNT(console) AS `Console`,
+  COUNT(total_sales) AS `Total Sales`,
+  COUNT( na_sales) AS `Na sales`,
+  COUNT(jp_sales) AS `jp_sales`,
+  COUNT(pal_sales) AS `pal_sales`,
+  COUNT( other_sales) AS ` other_sales`
+FROM
+  `videogames-sales.Videos_Games_Sales.GamesSales_Table`
+
+  
+## MOst sold games
+SELECT
+  title,
+  SUM(total_sales) AS `sales`
+FROM
+  `videogames-sales.Videos_Games_Sales.GamesSales_Table`
+  GROUP BY title
+ORDER BY
+  sales DESC
